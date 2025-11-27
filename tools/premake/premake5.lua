b787cad3d65409f249c07642c8975a6703f49571
corkiwang <corkiwang@tencent.com>
2025-11-27 10:47:06 +0800

    fix mac os compile error

--- tools/premake/premake5.lua
+++ tools/premake/premake5.lua
@@ -69,6 +69,14 @@ workspace ("llbc_" .. _ACTION)
         architecture(llbc_arch_type .. llbc_arch_connect_char .. '64')
     filter {}
 
+    -- ** 请将新的 macOS 部署目标版本设置添加到这里 **
+    filter { "system:macosx" }
+        -- 设置 macOS 最低部署目标版本，例如 10.15 (Catalina) 或更高。
+        -- 这样做是为了确保系统头文件（包括 fcntl.h）会暴露较新的宏如 F_PURGEFSYNC。
+        systemversion "10.15" 
+    filter {}
+    -- ** 结束添加 **
+    
     -- not use cxx11 abi.
     if llbc_disable_cxx11_abi then
         filter { "language:c++", "system:not windows" }
